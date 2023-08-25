38a146b2efb173bfe3b19494f4f7bb2af1c7f5b4
William Chen <wc1321@uw.edu>
2023-08-24 22:05:34 -0700

    made dropdown, displays results

--- amazon_request.py
+++ amazon_request.py
@@ -5,36 +5,57 @@ data = response.json()
 result = {}
 
 
-def get_station_code():
-    for routeid, info in data.items():
-        result[routeid] = {"Station code": info.get("station_code")}
-    return result
+def route_ids():
+    datakeys = list(data.keys())[:10]
+    return datakeys
 
-def get_departure_time():
-    for routeid, info in data.items():
-        result[routeid] = {"Departure Time": info.get("departure_time_utc")}
-    return result
+def get_stats(user_key_request):
+    return data[user_key_request]
 
-def get_stops():
-    for routeid, info in data.items():
-        result[routeid] = {"Stops: ": info.get("stops")}
-    return result
 
-def get_stop_location():
-    location = {}
-    for routeid, info in data.items():
-        result[routeid] = {"Stop ID": info.get("stops")}
-        for inital, stop_info in info["stops"].items():
+def get_station_code(user_key_request):
+    station_code = data[user_key_request]['station_code']
+    return station_code
+
+def get_departure_time(user_key_request):
+    departure_time = data[user_key_request]['departure_time_utc']
+    return departure_time 
+
+def get_stops(user_key_request):
+    stops= data[user_key_request]['stops']
+    return stops
+
+# def get_station_code():
+#     for routeid, info in list(data.items())[:10]:
+#         result[routeid] = {"Station code": info.get("station_code")}
+#     return result
+
+# def get_departure_time():
+#     for routeid, info in list(data.items())[:10]:
+#         result[routeid] = {"Departure Time": info.get("departure_time_utc")}
+#     return result
+
+# def get_stops():
+#     for routeid, info in data.items():
+#         result[routeid] = {"Stops: ": info.get("stops")}
+#     return result
+
+# def get_stop_location():
+#     location = {}
+#     for routeid, info in data.items():
+#         result[routeid] = {"Stop ID": info.get("stops")}
+#         for inital, stop_info in info["stops"].items():
             
-            lat = stop_info["lat"]
-            lng = stop_info['lng']
+#             lat = stop_info["lat"]
+#             lng = stop_info['lng']
         
-            location[inital] = {
-                "lat": lat, "lng": lng
-            }
-        result[routeid] = {'stops': location}
-    return result
+#             location[inital] = {
+#                 "lat": lat, "lng": lng
+#             }
+#         result[routeid] = {'stops': location}
+#     return result
         # result[routeid] = {"Initial":}
         # stop_locations = {}
         # for stops in info[""]
-get_stop_location()
+
+
--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
-from amazon_request import get_departure_time, get_station_code, get_stop_location, get_stops
+from flask import Flask, render_template, request
+from amazon_request import get_departure_time, get_station_code, get_stops, route_ids, data
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("home.html")
+@app.route('/', methods = ['GET', 'POST'])
+def dropdown():
+    datakeys = route_ids()
+    if request.method == 'POST':
+        str_route = (request.get_data().decode())
+        str_route_ess = (str_route[5:49])
+        all_stats = (data[str_route_ess])
+        print(all_stats)
+    return render_template("index.html", datakeys = datakeys, all_stats = all_stats)  
 
 @app.route('/time')
 def run_get_departure_time():
@@ -17,10 +23,10 @@ def run_get_station_code():
     data = get_station_code()
     return render_template("index.html", data = data)
 
-@app.route('/locations')
-def run_get_stop_location():
-    data = get_stop_location()
-    return render_template("index.html", data = data)
+# @app.route('/locations')
+# def run_get_stop_location():
+#     data = get_stop_location()
+#     return render_template("index.html", data = data)
 
 
 @app.route('/stop')
@@ -33,3 +39,4 @@ def run_get_stops():
 
 if __name__ == '__main__':
     app.run(debug = True)
+    
