7910c863697abf12be8f0798861af35f88def084
William Chen <wc1321@uw.edu>
2023-09-05 17:38:59 -0700

    trying to insert into table

--- app.py
+++ app.py
@@ -1,40 +1,102 @@
 from flask import Flask, render_template, request
-from amazon_request import route_ids, data, r
+import requests
+from amazon_request import route_ids, data
 import folium
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
+from configparser import ConfigParser
+from sqlalchemy import create_engine, ForeignKey, Column, Integer, String, Text, REAL, Sequence, Float
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+engine = create_engine('sqlite:///route.db', echo = True)
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
-conn = sqlite3.connect("city.db")
-cursor = conn.cursor()
+Session = sessionmaker(bind = engine)
+session = Session()
+
+config_object = ConfigParser()
+config_object.read("config.ini")
+api_stuff = config_object["api_info"]
+api_key = api_stuff["api_key"]
+url = api_stuff["url"]
+conn = sqlite3.connect("route.db")
+c = conn.cursor()
 datakeys = route_ids() #datakeys is a list
 singlecitywithstops = []    
 city_list = []
 state_list = []
 place = [] 
-str_route = (request.get_data().decode())
-str_route_ess = (str_route[5:49])
-all_stats = (data[str_route_ess]) 
-json_data = r.json()
-stat = data[curr_route] #used to pull from the s3
+# str_route = (request.get_data().decode())
+# str_route_ess = (str_route[5:49])
+# all_stats = (data[str_route_ess]) 
+source = ''
+destination = ''
+# r = requests.get(url+ "origins=" + source + "&destinations=" + destination + "&key=" + api_key)
+# json_data = r.json()
+# stat = data[curr_route] #used to pull from the s3
 
 
 
-def populate_drop():
-  
-    for i in range(len(datakeys)):
-        curr_route = datakeys[i]
-        for destinations in stat['stops'].values():
-            singlecitywithstops.append(str(destinations['lat']) + ", " +str(destinations['lng']) +"|")
-        for i in range(0, len(singlecitywithstops) -2, 2):
-            source = singlecitywithstops[i]
-            destination = singlecitywithstops[i+2]
-        conn.commit()
-        conn.close()
+
+
+Base = declarative_base()
+class table(Base):
+    __tablename__ = "routes"
+    route_id = Column(Integer, primary_key=True)
+    route_number = Column(Integer, Sequence('route_number_seq'), unique=True)
+    stop_id = Column(Text)
+    stop_lat = Column(Float)
+    stop_lng = Column(Float)
+    route_sq = Column(Text)
+    city = Column(Text)
+
+    def __init__(self, route_id, stop_id, stop_lat, stop_lng, route_sq, city):
+        self.route_id = route_id
+        self.stop_id = stop_id
+        self.stop_lat = stop_lat
+        self.stop_lng = stop_lng
+        self.route_sq = route_sq
+        self.city = city
+
+
+
+def insert_from_aws():
+    for route_id in datakeys:
+        route_data = data[route_id]        
+        # Insert or update stops for the route
+        for stop_id, stop_info in route_data["stops"].items():
+            # Initialize the 'table' class with the required parameters
+            stop = table(route_id=route_id, stop_id=stop_id, stop_lat=(stop_info['lat']), stop_lng=(stop_info['lng']), route_sq=None, city=None)
+            session.add(stop)
+        # Commit changes to the database after processing each route
+        session.commit()
+
+    # Close the session at the end
+    session.close()
+
+        
+
+# def insert_google():
+#     records = session.query(table.stop_lat, table.stop_lng).all()
+#     for stop_lat, stop_lng in records:
+#         print(stop_lng, stop_lat)
+
+
+# def populate_drop():
+    
+#     for i in range(len(datakeys)):
+#         curr_route = datakeys[i]
+#         c.execute("INSERT INTO city (routeid) VALUES (curr_route) VALUES (?)", (curr_route))
+#         for destinations in stat['stops'].values():
+#             singlecitywithstops.append(str(destinations['lat']) + ", " +str(destinations['lng']) +"|")
+#         for i in range(0, len(singlecitywithstops) -2, 2):
+#             source = singlecitywithstops[i]
+#             destination = singlecitywithstops[i+2]
+#         conn.commit()
+#         conn.close()
            
-
 def create_map():
     feature_group = folium.FeatureGroup("Locations")
     for name, stop_details in all_stats['stops'].items():
@@ -88,15 +150,30 @@ def dropdown():
 
     return render_template("index.html", datakeys = datakeys)  
 
+# def create_table(conn, create_table_sql):
+#     """ create a table from the create_table_sql statement
+#     :param conn: Connection object
+#     :param create_table_sql: a CREATE TABLE statement
+#     :return:
+#     """
+#     try:
+#         c = conn.cursor()
+#         c.execute(create_table_sql)
+#     except Error as e:
+#         print(e)
 
 
 
 if __name__ == '__main__':
     # sqlstuff()
-    populate_drop()
-
-    # app.run(debug = True)
+    # create_table(conn, table)
+#    Base.metadata.create_all(engine)
+    # print('done with aws insert, google now')
+    insert_from_aws()
+    # print('done with google insert')
+        # app.run(debug = True)
     # create_map()
+    
     
 
 # datakeys are  the routeID titles
